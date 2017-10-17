919c440633c149b29253b2906bff856346ae7c59
ruiehq <seocored@gmail.com>
2017-10-17 12:39:40 +0800

    Modified serverjs

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
