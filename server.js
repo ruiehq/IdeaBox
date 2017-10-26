347e757e235a32ef23987919788f5385f1967ce2
ruiehq <seocored@gmail.com>
2017-10-26 18:49:12 +0800

    Added models of idea & fix /ideas routes

--- server.js
+++ server.js
@@ -2,13 +2,40 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
-const PORT = 3000;
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/ideabox', { useMongoClient: true })
+   .then(() => console.log('MongoDB Connected'))
+   .catch(err => console.log(err));
 
 // Middlewares
 app.engine('handlebars', hbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(session({
+   secret: 'secret',
+   resave: true,
+   saveUninitialized: true
+}))
+app.use(flash());
+
+// Global Middlewares
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+
+});
+
+// Models
+const Idea = require('./models/Idea.js');
 
 // Routes
 app.get('/', (req, res) => {
@@ -17,10 +44,45 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req ,res) => {
    res.render('about');
-})
+});
 
+app.get('/ideas', (req, res) => {
+   Idea.find({})
+      .sort({ date: 'desc' })
+      .then(ideas => {
+         res.render('ideas/index', { ideas });
+      })
+      .catch(err => console.log(err));
+});
+
+app.post('/ideas', (req, res) => {
+   const { title, details } = req.body;
+   const newUser = {
+      title,
+      details
+   };
+   new Idea(newUser)
+      .save()
+      .then(idea => {
+         req.flash('success_msg', 'Idea added successfully');
+         res.redirect('/ideas');
+      });
+});
+
+app.delete('/ideas/:id', (req, res) => {
+   Idea.remove({ _id: req.params.id })
+      .then(() => {
+         req.flash('success_msg', 'Idea Removed Successfully');
+         res.redirect('/ideas');
+      })
+});
+
+app.get('/ideas/add', (req, res) => {
+   res.render('ideas/add');
+});
 
 // Run Server
+const PORT = 3000;
 app.listen(PORT, () => {
    console.log(`App is running on PORT:${PORT}`);
 })
